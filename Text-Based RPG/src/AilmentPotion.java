4285de3d9c4916582c69609e3f3d989f465688fd
Manzkii <107500560+Manzkii@users.noreply.github.com>
2025-04-03 22:08:12 +0800

    Changed MiscellaneousPotion to AilmentPotion

---
+++ Text-Based RPG/src/AilmentPotion.java
@@ -0,0 +1,37 @@
+class AilmentPotion extends Item{
+    private final int AilmentAmount;
+
+
+    private AilmentPotion(String name, String description, int AilmentAmount ) {
+        super(name, description);
+        this.AilmentAmount = AilmentAmount;
+    }
+
+
+    public void use (Player player) {
+        player.atk += AilmentAmount;
+
+        System.out.println("Using a " + name + ", it's a Ailment Potion " + AilmentAmount);
+    }
+
+    public static AilmentPotion Antidote(){
+        return new AilmentPotion("Antidote", "Cures Poison.", 50 );
+    }
+
+    public static AilmentPotion EyeDrops(){
+        return new AilmentPotion("EyeDrops", "Cures Blind.", 50);
+    }
+
+    public static AilmentPotion EchoGrass(){
+        return new AilmentPotion("Echo Grass", "Cures Silence.", 50);
+    }
+
+    public static AilmentPotion HolyWater(){
+        return new AilmentPotion("Holy Water", "Cures Zombie and Curse.", 50);
+    }
+
+    public static AilmentPotion Remedy(){
+        return new AilmentPotion("Remedy", "Cures most status effects (except Doom and Charm).", 50);
+    }
+
+}
