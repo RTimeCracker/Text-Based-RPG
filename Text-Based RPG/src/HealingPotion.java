07cf508570a3b3aa2006b058b0eef4f3f69fb5b0
Ryan Llano <llanoryan02@gmail.com>
2025-04-04 19:42:11 +0800

    HealingPotion, HybridPotion, and ManaPotion edited

--- Text-Based RPG/src/HealingPotion.java
+++ Text-Based RPG/src/HealingPotion.java
@@ -18,11 +18,14 @@ class HealingPotion extends Item{
     }
 
     public static HealingPotion superPotion(){
-        return new HealingPotion("Super Potion", "Heals for a greater amount.", 150);
+        return new HealingPotion("Super Potion", "Heals for a greater amount.", 100);
     }
 
     public static HealingPotion maxPotion(){
-        return new HealingPotion("Max Potion", "Heals for a max amount.", 250);
+        return new HealingPotion("Max Potion", "Heals for a max amount.", 200);
     }
 
+    public static HealingPotion PhoenixDownPotion(){
+        return new HealingPotion("Phoenix Down Potion", "Revives a KO'd unit with 10 HP.", 10);
+    }
 }
---
+++ Text-Based RPG/src/ManaPotion.java
@@ -0,0 +1,27 @@
+class ManaPotion extends Item{
+    private final int ManaAmount;
+
+    private ManaPotion(String name, String description, int ManaAmount) {
+        super(name, description);
+        this.ManaAmount = ManaAmount;
+    }
+
+    
+    public void use (Player player) {
+        player.hp += ManaAmount;
+
+        System.out.println("Using a " + name + ", it restores HP and MP " + ManaAmount + " HP & MP!");
+    }
+
+    public static ManaPotion LowlyFlaskofTears(){
+        return new ManaPotion("Lowly Flask of Tears", "Restores 20 MP.", 50);
+    }
+
+    public static ManaPotion GreatFlaskofDemiGodTears(){
+        return new ManaPotion("Great Flask of DemiGod Tears", "Restores 70 MP.", 100);
+    }
+
+    public static ManaPotion AbsoluteFlaskofGodTears(){
+        return new ManaPotion("Absolute Flask of God Tears", "Restores 150 MP.", 200);
+    }
+}
